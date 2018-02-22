094ac4b238f1e83bdd508cc285aaeaf76a7b4ab4
Luke H <LukeH676@users.noreply.github.com>
2018-02-22 13:16:33 -0500

    Initial commit

---
+++ app/src/main/java/comp208/ca/on/sl/timerdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package comp208.ca.on.sl.timerdemo;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
