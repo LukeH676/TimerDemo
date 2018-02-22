1819b72c7ea823cc784ad1bffef1b1b81e5a968b
Luke H <LukeH676@users.noreply.github.com>
2018-02-22 14:30:59 -0500

    Fin

--- app/src/main/java/comp208/ca/on/sl/timerdemo/MainActivity.java
+++ app/src/main/java/comp208/ca/on/sl/timerdemo/MainActivity.java
@@ -1,11 +1,14 @@
 package comp208.ca.on.sl.timerdemo;
 
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.support.annotation.BoolRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -14,6 +17,18 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
     SeekBar timeSeekbar;
     TextView timerText;
+    Button timerBtn;
+    Boolean counterIsActive = false;
+    CountDownTimer countDownTimer;
+
+    public void resetTimer(){
+        timerText.setText("0:30");
+        timeSeekbar.setProgress(30);
+        countDownTimer.cancel();
+        timerBtn.setText("Go!");
+        timeSeekbar.setEnabled(true);
+        counterIsActive = false;
+    }
 
     public void updateTimer(int secondsLeft){
         int minutes = (int) secondsLeft / 60;
@@ -21,27 +36,40 @@ public class MainActivity extends AppCompatActivity {
 
         String secondString = Integer.toString(seconds);
 
-        if (secondString.equals("0")){
-            secondString = "00";
+     if (seconds <= 9){
+            secondString = "0" + secondString;
         }
         timerText.setText(Integer.toString(minutes)+ ":"+secondString);
 
     }
 
     public void controlTimer(View view){
-        Log.i("button Pressed","Pressed");
-        new CountDownTimer(timeSeekbar.getProgress()* 1000,1000){
 
-            @Override
-            public void onTick(long millisUntilFinished) {
-             updateTimer((int) millisUntilFinished / 1000);
-            }
+        if(counterIsActive == false) {
+            Log.i("button Pressed","Pressed");
+            counterIsActive = true;
+            timeSeekbar.setEnabled(false);
+            timerBtn.setText("Stop");
+            countDownTimer = new CountDownTimer(timeSeekbar.getProgress() * 1000 + 100, 1000) {
 
-            @Override
-            public void onFinish() {
-            Log.i("finished","done");
-            }
-        }.start();
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer((int) millisUntilFinished / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+                    timerText.setText("0:00");
+                    resetTimer();
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                    mediaPlayer.start();
+                    Log.i("finished", "done");
+                }
+            }.start();
+        }else {
+        resetTimer();
+
+        }
     }
 
     @Override
@@ -54,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
         timerText = (TextView) findViewById(R.id.time);
         timeSeekbar.setMax(600);
         timeSeekbar.setProgress(30);
+        timerBtn = (Button) findViewById(R.id.timerBtn);
         timeSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
